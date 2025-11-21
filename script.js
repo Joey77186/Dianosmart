a4ef3edcc677080aeada53e0eca8fd42a28ffeda
JoeyIshere <y7229292@gmail.com>
2025-11-21 16:10:30 +0200

    index.html

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("goBtn");
+
+  function goToPage() {
+    window.location.href = "Arabic_index.html"; 
+  }
+
+  btn.addEventListener("click", goToPage);
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("goBttn");
+
+  function goToPage() {
+    window.location.href = "index.html"; 
+  }
+
+  btn.addEventListener("click", goToPage);
+});
